f4179b88e9509be5432f2c20552b17a62764b106
Arvind <arvind19946@gmail.com>
2016-10-11 18:13:46 +0530

    Removed unwanted files

--- HowdyDoc/www/js/controllers.js
+++ HowdyDoc/www/js/controllers.js
@@ -45,17 +45,3 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
     $state.go("app.dashboard");
   }
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
